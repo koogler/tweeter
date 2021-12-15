447f75d3d465765a8c7c052861eebb1f65e25614
Desmond Parker <evan.parker95@gmail.com>
2021-12-15 03:51:42 +0000

    found out how to find counter class through jquery, and made it so that it's number changes as characters fill the .textBox area. also added an if statement to change the number's color to red if its value becomes negative.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
   $(".textBox").on("input", function () {
+    let findCounter = $(this).parent().find(".counter")
+    let remaining = 140
     let currentText = $(this).val().length
-    console.log(currentText)
+    let charCount = (remaining - currentText)
+    console.log(charCount)
+    findCounter.text(charCount)
+    if (charCount < 0) {
+      findCounter.css("color", "red")
+    }
   });
   console.log('test123')
 });
